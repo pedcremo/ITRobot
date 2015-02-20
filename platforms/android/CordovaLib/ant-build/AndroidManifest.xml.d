00b4036daf5a54988174ef2558de4ada790097c6
nicklaus_ <pedcremo@gmail.com>
2015-02-20 18:21:12 +0100

    Proves amb phonegap build

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/pedcremo/ITRobot/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/home/pedcremo/ITRobot/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/pedcremo/ITRobot/platforms/android/ant-build/AndroidManifest.xml : \
+/home/pedcremo/ITRobot/platforms/android/AndroidManifest.xml \
