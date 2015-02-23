6de853ae43ee0b7ad1e94b727c5c647b10bd8c9e
nicklaus_ <pedcremo@gmail.com>
2015-02-23 16:27:41 +0100

    Refactor to jquery mobile

--- www/js/index.js
+++ www/js/index.js
@@ -1,74 +1,178 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+    Authored by Pere Crespo
+
+    Based on Chat Example for Bluetooth Serial PhoneGap Plugin
+    http://github.com/don/BluetoothSerial
+
+    Copyright 2013 Don Coleman
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/* jshint quotmark: false, unused: vars */
+/* global cordova, bluetoothSerial, listButton, connectButton, sendButton, disconnectButton */
+/* global chatform, deviceList, message, messages, statusMessage, chat, connection */
+'use strict';
+
 var app = {
-    // Application Constructor
     initialize: function() {
-        this.bindEvents();
+        this.bind();
+        listButton.style.display = "none";
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+    bind: function() {
+        document.addEventListener('deviceready', this.deviceready, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        // check to see if Bluetooth is turned on.
-        // this function is called only
-        //if isEnabled(), below, returns success:
-        var listPorts = function() {
-            // list the available BT ports:
-            bluetoothSerial.list(
-                function(results) {
-                    app.display(JSON.stringify(results));
-                },
-                function(error) {
-                    app.display(JSON.stringify(error));
-                }
-            );
+    deviceready: function() {
+        // note that this is an event handler so the scope is that of the event
+        // so we need to call app.foo(), and not this.foo()
+
+        // wire buttons to functions
+        connectButton.ontouchstart = app.connect;
+        listButton.ontouchstart = app.list;
+
+        sendButton.ontouchstart = app.sendData;
+        chatform.onsubmit = app.sendData;
+        disconnectButton.ontouchstart = app.disconnect;
+
+        // listen for messages
+        bluetoothSerial.subscribe("\n", app.onmessage, app.generateFailureFunction("Subscripció fallida"));
+
+        // get a list of peers
+        setTimeout(app.list, 2000);
+    },
+    list: function(event) {
+        deviceList.firstChild.innerHTML = "Descobrint...";
+        app.setStatus("Cercant dispositius Bluetooth ...");
+        bluetoothSerial.list(app.ondevicelist, app.generateFailureFunction("Llistat Fallit"));
+    },
+    connect: function() {
+        var device = deviceList[deviceList.selectedIndex].value;
+        app.disable(connectButton);
+        app.setStatus("Connectant...");
+        console.log("Sol·licitant connexió a " + device);
+        bluetoothSerial.connect(device, app.onconnect, app.ondisconnect);
+    },
+    disconnect: function(event) {
+        if (event) {
+            event.preventDefault();
         }
 
-        // if isEnabled returns failure, this function is called:
-        var notEnabled = function() {
-            app.display("Bluetooth is not enabled.")
+        app.setStatus("Desconnectant...");
+        bluetoothSerial.disconnect(app.ondisconnect);
+    },
+    sendData: function(event) {
+        event.preventDefault();
+
+        var text = message.value + "\n";
+        var success = function () {
+            message.value = "";
+            messages.value += ("Us: " + text);
+            messages.scrollTop = messages.scrollHeight;
+        };
+
+        bluetoothSerial.write(text, success);
+        return false;
+    },
+    ondevicelist: function(devices) {
+        var option;
+
+        // remove existing devices
+        deviceList.innerHTML = "";
+        app.setStatus("");
+
+        devices.forEach(function(device) {
+
+            option = document.createElement('option');
+            if (device.hasOwnProperty("uuid")) {
+                option.value = device.uuid;
+            } else if (device.hasOwnProperty("address")) {
+                option.value = device.address;
+            } else {
+                option.value = "ERROR " + JSON.stringify(device);
+            }
+            option.innerHTML = device.name;
+            deviceList.appendChild(option);
+        });
+
+        if (devices.length === 0) {
+
+            option = document.createElement('option');
+            option.innerHTML = "No dispositius Bluetooth ";
+            deviceList.appendChild(option);
+
+            if (cordova.platformId === "ios") { // BLE
+                app.setStatus("No periferics Bluetooth Descoberts.");
+            } else { // Android
+                app.setStatus("Per favor emparella un dispositiu bluetooth.");
+            }
+
+            app.disable(connectButton);
+            listButton.style.display = "";
+        } else {
+            app.enable(connectButton);
+            listButton.style.display = "none";
+            app.setStatus("Trobat " + devices.length + " dispositiu" + (devices.length === 1 ? "." : "s."));
         }
 
-         // check if Bluetooth is on:
-        bluetoothSerial.isEnabled(
-            listPorts,
-            notEnabled
-        );
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    onconnect: function() {
+        //connection.style.display = "none";
+        $.mobile.changePage("#pagetwo");
+        chat.style.display = "block";
+        app.setStatus("Connectat");
+    },
+    ondisconnect: function(reason) {
+        var details = "";
+        if (reason) {
+            details += ": " + JSON.stringify(reason);
+        }
+        connection.style.display = "block";
+        app.enable(connectButton);
+        chat.style.display = "none";
+        app.setStatus("Desconnectat");
+    },
+    onmessage: function(message) {
+        messages.value += "Them: " + message;
+        messages.scrollTop = messages.scrollHeight;
+    },
+    setStatus: function(message) { // setStatus
+        console.log(message);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        window.clearTimeout(app.statusTimeout);
+        statusMessage.innerHTML = message;
+        statusMessage.className = 'fadein';
 
-        console.log('Received Event: ' + id);
+        // automatically clear the status with a timer
+        app.statusTimeout = setTimeout(function () {
+            statusMessage.className = 'fadeout';
+        }, 5000);
+    },
+    enable: function(button) {
+        button.className = button.className.replace(/\bis-disabled\b/g,'');
+    },
+    disable: function(button) {
+        if (!button.className.match(/is-disabled/)) {
+            button.className += " is-disabled";
+        }
+    },
+    generateFailureFunction: function(message) {
+        var func = function(reason) { // some failure callbacks pass a reason
+            var details = "";
+            if (reason) {
+                details += ": " + JSON.stringify(reason);
+            }
+            app.setStatus(message + details);
+        };
+        return func;
     }
 };
